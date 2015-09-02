1132c0f96cf6200fa2b572ff857233f80855268e
bosp1301 <atkinson.unt@gmail.com>
2015-09-02 09:23:26 -0500

    Create blank.c

---
+++ blank.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* 
+Program name
+program function
+Cody Atkinson
+Date
+*/
+void definethings(void){        //beginning of definition
+
+
+			}				//end of definition
+
+
+void main(void){                //beginning of main
+
+
+		}			        //end of main
