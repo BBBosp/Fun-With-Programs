02c1f5d300f5f0f31ae7f4fcf8630ea191820744
bosp1301 <atkinson.unt@gmail.com>
2015-09-02 09:32:28 -0500

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###########################
+#hello world test program
+#Name
+#February 23, 2015
+###########################
+#declare string as variable
+string="hello world"
+#print declared variable
+echo $string
